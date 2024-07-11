8197c73203dd429d163e57361175b7ae50c74f62
Vikas Yadav <vikashyadav62502@gmail.com>
2024-07-11 19:07:41 +0530

    Setup and varibales done

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x: i32 = -10;
+    let positive_integer: u32 = 10;
+    let float_number: f32 = 10.0;
+    print!("Vikas Yadav");
+
+    print!(": {}", x);
+    print!(": {}", positive_integer);
+    print!(": {}", float_number);
+}
