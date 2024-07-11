6950483f36bc859618557be5099ad5e9ba99d4e4
Vikas Yadav <vikashyadav62502@gmail.com>
2024-07-11 19:39:07 +0530

    Update main.rs
    learned boolean and string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // variable integer
     let x: i32 = -10;
     let positive_integer: u32 = 10;
     let float_number: f32 = 10.0;
@@ -7,4 +8,25 @@ fn main() {
     print!(": {}", x);
     print!(": {}", positive_integer);
     print!(": {}", float_number);
+
+    // Boolean
+    let is_male: bool = true;
+    let is_age_less_than_18: bool = true;
+
+    // if else condition
+    if is_male {
+        println!("Your are a male bro")
+    }
+
+    if is_age_less_than_18 {
+        println!("You are kid")
+    }
+
+    if is_male && is_age_less_than_18 {
+        println!("You are kid as well as male")
+    }
+
+    // string
+    let greeting: String = String::from("String declared using String::from()");
+    println!("{}", greeting);
 }
